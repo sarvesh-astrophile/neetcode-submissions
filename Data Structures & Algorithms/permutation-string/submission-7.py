bfb8325812fd7e0c125767785ff31e474fd6ea58
Sarvesh Kumar <65073069+sarvesh-astrophile@users.noreply.github.com>
2026-06-15 11:25:13 +0530

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,52 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        # init
+        s1_26, s2_26 = [0] * 26, [0] * 26
+        matches = 0
+
+        for i in range(len(s1)):
+            index1 = ord(s1[i]) - ord('a')
+            s1_26[index1] += 1
+
+            index2 = ord(s2[i]) - ord('a')
+            s2_26[index2] += 1
+
+        for i in range(26):
+            if s1_26[i] == s2_26[i]:
+                matches += 1
+
+        # algo
+        l = 0
+        for r in range(len(s1), len(s2)):
+
+            if matches == 26:
+                return True
+            
+            # for right
+            right_index = ord(s2[r]) - ord('a')
+            if s1_26[right_index] == s2_26[right_index]:
+                matches -= 1
+
+            s2_26[right_index] += 1
+            if s1_26[right_index] == s2_26[right_index]:
+                matches += 1
+
+            # for left
+
+            left_index = ord(s2[l]) - ord('a')
+            if s1_26[left_index] == s2_26[left_index]:
+                matches -= 1
+
+            s2_26[left_index] -= 1
+            if s1_26[left_index] == s2_26[left_index]:
+                matches += 1
+
+            l += 1
+            
+
+
+        return matches == 26
+            
