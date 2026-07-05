086a7784cd3dfa110220006509c4da716119e36b
Sarvesh Kumar <65073069+sarvesh-astrophile@users.noreply.github.com>
2026-07-05 12:22:37 +0530

    Add: permutation-string - submission-15

---
+++ Data Structures & Algorithms/permutation-string/submission-15.py
@@ -0,0 +1,44 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+            
+        s1_26, s2_26 = [0] * 26, [0] * 26
+        matches = 0
+
+        for i in range(len(s1)):
+            index1 = ord(s1[i]) - ord("a")
+            s1_26[index1] += 1
+
+            index2 = ord(s2[i]) - ord("a")
+            s2_26[index2] += 1
+
+        for i in range(26):
+            if s1_26[i] == s2_26[i]:
+                matches += 1
+
+        # algo
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+
+            index1 = ord(s2[r]) - ord("a")
+            if s1_26[index1] == s2_26[index1]:
+                matches -= 1
+
+            s2_26[index1] += 1
+            if s1_26[index1] == s2_26[index1]:
+                matches += 1
+
+            index2 = ord(s2[l]) - ord("a")
+            if s1_26[index2] == s2_26[index2]:
+                matches -= 1
+
+            s2_26[index2] -= 1
+            if s1_26[index2] == s2_26[index2]:
+                matches += 1
+
+            l += 1
+
+        return matches == 26
